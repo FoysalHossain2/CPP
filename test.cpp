14d09aac265212b0d76c855eadebb976105c0afa
FoysalHossain2 <fh375170@gmail.com>
2026-02-04 23:49:55 +0600

    frist commit in CPP

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x = 100;
+    char c = 'A';
+    double d = 34.56;
+
+    std:: cout << x << c << d << "\n";
+    // 
+    std:: cout << "My favorite numbner is" << x << std::endl;
+    return 0;
+}
