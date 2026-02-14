fe9c38954a281fe4d112669ca45ccd6c5c576aac
FoysalHossain2 <fh375170@gmail.com>
2026-02-14 21:41:20 +0600

    switch

---
+++ switch3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int c;
+    cin >> c;
+    switch (c)
+    {
+        case 'a':
+            cout << "Vowel\n";
+            break;
+        case 'e':
+           cout << "Vowel\n";
+            break;
+        case 'i':
+           cout << "Vowel\n";
+            break;
+        case 'o':
+           cout << "Vowel\n";
+            break;
+        case 'u':
+            cout << "Vowel\n";
+            break;
+        default:
+            cout << "Consonant";
+    }
+
+    return 0;
+} 
