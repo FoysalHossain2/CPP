b88a7729e9db9b0dc4d51c4ca5a0053fa452323e
FoysalHossain2 <fh375170@gmail.com>
2026-02-14 23:04:18 +0600

    min_max_swap

---
+++ min_max_swp.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    // if (a<b)
+    // {
+    //     cout << a << endl;
+    // }
+    // else 
+    // {
+    //     cout << b << endl;
+    // }
+
+    // cout << min({3,5,7,10,12}) << endl;
+    // cout << max({233,45,677,899,}) << endl;
+    
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    swap(a,b);
+    cout << a << " " << b << endl;
+
+    return 0;
+}
