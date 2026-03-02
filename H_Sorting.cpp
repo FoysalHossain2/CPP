f71acd8a0a664d9494e5d108176295a50480d28e
FoysalHossain2 <fh375170@gmail.com>
2026-03-02 07:11:24 +0600

    H. Sorting in cpp

--- H_Sorting.cpp
+++ H_Sorting.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int a[n];
+    int temp;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " " ;
+    }
+    
     
     return 0;
 }
