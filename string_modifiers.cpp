812b010627e5b590761131b877ab9f635d68dd2b
FoysalHossain2 <fh375170@gmail.com>
2026-03-09 01:07:45 +0600

    string_modifiers

---
+++ string_modifiers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+
+    // s += s2;
+    // s.append(s2)
+
+    // s.push_back('A');
+    // s += 'B';
+
+    s.pop_back();
+    s.pop_back();
+
+    cout << s << endl;
+    return 0;
+}
