ed8d9165ca9568f05d677e9586a9da5a6b18fb69
FoysalHossain2 <fh375170@gmail.com>
2026-03-11 00:33:09 +0600

    string_input_with_spaces

---
+++ string_input_with_spaces.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    string s;
+    // getline(cin, s);
+    cin >> s;
+    cout << x << endl;
+    cout << s << endl;
+    return 0;
+}
