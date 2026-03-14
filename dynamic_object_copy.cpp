be771cff7b58a82530da080dcd354ea715eafad2
FoysalHossain2 <fh375170@gmail.com>
2026-03-14 23:42:29 +0600

    done

---
+++ dynamic_object_copy.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Cricketer
+{
+    public:
+    string country;
+    int jersey;
+
+    Cricketer(string country, int jersey)
+    {
+        this->country = country;
+        this->jersey = jersey;
+    }
+};
+
+int main()
+{
+    Cricketer* dhoni = new Cricketer("India", 100);
+    Cricketer* kholi = new Cricketer("India", 18);
+    kholi = dhoni;
+    delete dhoni;
+    cout << dhoni->jersey << " " << kholi->jersey << endl;
+    return 0;
+}
