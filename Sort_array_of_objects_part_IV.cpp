22ee1997e686dcaafa921b8ce5e3642c9bde0058
FoysalHossain2 <fh375170@gmail.com>
2026-04-14 02:58:40 +0600

    done

--- Sort_array_of_objects_part_IV.cpp
+++ Sort_array_of_objects_part_IV.cpp
@@ -11,24 +11,39 @@ class Student
 
 bool cmp(Student l, Student r) 
 {
-    if (l.marks > r.marks)
-    {
-        return true;
-    } 
-    else if(l.marks < r.marks){
-        return false;
-    }
-    else
-    {
-        // if(l.roll < r.roll)
-        // {
-        //     return true;
-        // }
-        // else{
-        //     return false;
-        // }
-        return l.roll < r.roll;  //this is sortcart aproch 
-    }
+
+   return l.marks == r.marks ? l.roll < r.roll : l.marks > r.marks;
+
+    // if (l.marks == r.marks)
+    // {
+    //     return l.roll < r.roll;
+    // }
+    // else
+    // {
+    //     return l.marks > r.marks;
+    // }
+    
+    // if (l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+    // else if (l.marks < r.marks)
+    // {
+    //     return false;
+    // }
+    // else
+    // {
+    //     if(l.roll < r.roll)
+    //     {
+    //         return true;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
+    
+
 }
 
 int main()
