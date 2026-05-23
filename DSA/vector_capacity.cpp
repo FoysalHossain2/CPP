d71ac088f5dca6b4234a6f1c0f4ea50734316d46
Md.Foysal Hossain <fh375170@gmail.com>
2026-05-23 22:32:17 +0600

    done

---
+++ DSA/vector_capacity.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> v;
+    cout << v.capacity() << endl; // capacity of vector
+    v.push_back(10);
+    cout << v.capacity() << endl; // capacity of vector
+    v.push_back(10);
+    cout << v.capacity() << endl; // capacity of vector
+    v.push_back(10);
+    cout << v.capacity() << endl; // capacity of vector
+    v.push_back(10);
+    cout << v.capacity() << endl; // capacity of vector
+    v.push_back(10);
+    cout << v.capacity() << endl; // capacity of vector
+
+    return 0;
+}
