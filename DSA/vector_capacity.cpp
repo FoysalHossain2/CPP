92f87ff2633f15ebeb435211d88ef3d7c615778a
Md.Foysal Hossain <fh375170@gmail.com>
2026-08-27 11:44:17 +0600

    Refactor Y_Range_sum_query to use long long for vector and improve sum calculation logic

--- DSA/vector_capacity.cpp
+++ DSA/vector_capacity.cpp
@@ -7,15 +7,13 @@ int main()
     vector<int> v;
     cout << v.capacity() << endl; // capacity of vector
     v.push_back(10);
-    cout << v.capacity() << endl; // capacity of vector
+    cout << v.capacity() << endl;
     v.push_back(10);
-    cout << v.capacity() << endl; // capacity of vector
+    cout << v.capacity() << endl;
     v.push_back(10);
-    cout << v.capacity() << endl; // capacity of vector
+    cout << v.capacity() << endl;
     v.push_back(10);
-    cout << v.capacity() << endl; // capacity of vector
-    v.push_back(10);
-    cout << v.capacity() << endl; // capacity of vector
+    cout << v.capacity() << endl;
 
     return 0;
 }
--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -5,21 +5,41 @@ int main()
 {
     int n,q;
     cin >> n >> q;
-    vector<int> v(n + 1);
+    vector<long long> v(n + 1);
     for (int i = 1; i <= n; i++)
     {
         cin >> v[i];
     }
 
+    vector<long long> pre(n+1);
+    pre[1] = v[1];
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i-1] + v[i];
+    }
+    
+    for (int i = 1; i <=n; i++)
+    {
+        cout << pre[i] << " ";
+    }
+    
+
     while (q--)
     {
         int l, r;
-        cin >> 1 >> r;
-        let sum = 0;
-        for (int i = l; i < =r; i++)
+        cin >> l >> r;
+        long long int sum;
+
+        if (l==1)
         {
-            sum += v[i];
+            sum = pre[r];
         }
+        else
+        {
+             sum = pre[r] - pre[l-1];
+        }
+        
+        
         cout << sum << endl;
     }
     
