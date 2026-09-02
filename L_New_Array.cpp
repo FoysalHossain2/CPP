d9a90683978da3e42555fb250c602f0617d48ca1
Md.Foysal Hossain <fh375170@gmail.com>
2026-09-03 00:06:00 +0600

    Add L. New Array problem solution with input/output handling

---
+++ L_New_Array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> newArray(vector<int> A, vector<int> B){
+    vector<int> C;
+    for(int x : A) C.push_back(x);
+    for(int x : B) C.push_back(x);
+    return C;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> B[i];
+    }
+
+    vector<int> C = newArray(B, A);
+    
+    for(int x : C)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
